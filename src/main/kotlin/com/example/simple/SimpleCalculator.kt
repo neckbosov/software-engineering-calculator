cb4ad56fd9d15c7f8792a33abeda8028a308d6bb
Andrei Kislitsyn <kisandy@yandex.ru>
2021-09-08 23:37:05 +0300

    fix unary minus

--- src/main/kotlin/com/example/simple/SimpleCalculator.kt
+++ src/main/kotlin/com/example/simple/SimpleCalculator.kt
@@ -8,11 +8,29 @@ import com.notkamui.keval.KevalInvalidExpressionException
 import com.notkamui.keval.KevalZeroDivisionException
 import com.example.models.AbstractDB
 
+fun String.insert(index: Int, value: String): String {
+    return take(index) + value + takeLast(length - index)
+}
+
+fun String.fixUnaryMinus(): String {
+    var res = this
+    for ((index, value) in this.withIndex()) {
+        if (value == '-') {
+            if (index == 0) {
+                res = res.insert(index, "0")
+            } else if (this[index-1] == '(') {
+                res = res.insert(index, "0")
+            }
+        }
+    }
+    return res
+}
+
 class SimpleCalculator(val db: AbstractDB) : AbstractCalculator {
     override fun calculate(stringExpression: String): Double {
+        val unaryMinusFixedExpression = stringExpression.fixUnaryMinus()
         try {
             val result = Keval.eval(stringExpression)
-            println("NO-OP calculator: ${stringExpression} = %d!".format(result))
             db.insert(stringExpression, result)
             return result
         } catch (e: KevalZeroDivisionException) {
